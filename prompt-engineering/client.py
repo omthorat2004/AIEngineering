624ed344dc34a40da3cac67e261ca78ef1b60c2c
Om Thorat <omthorat1005@gmail.com>
2026-06-01 23:57:42 -0700

    prompt-engineering: remove .DS_Store files

---
+++ prompt-engineering/client.py
@@ -0,0 +1,41 @@
+# Types of basic prompting
+# 1. Zero shot
+# 2. One shot
+# 3. Few shot
+# 4. Role based
+# 5. Reframing
+# 6. Combination based
+
+# Advanced prompting
+
+# 1. Chain of thought
+# We define how model is going to think one by one through the prompt. Use human intelligence, use in the complex processes
+# we only use it for #reasoning
+
+#2. Self Consistency
+# Enhancement of Chain of thought by generating multiple reasoning paths. This is use when problems have various solution methods. It is used top check consistency of an answer
+
+#3. RaAct Prompting
+# ReAct combines:
+
+# Reasoning + Taking actions (like using tools, APIs, search, databases)
+
+# The model alternates between:
+
+# Thought (reasoning)
+# Action (tool call)
+# Observation (result from tool)
+# We use it for reasoning + actions
+
+#4. Multimodal prompting
+# Multimodal prompt engineering involves crafting prompts that work with multiple types of input data. This technique enables more comprehensive and context-aware AI interactions by combining different forms of information.
+
+
+#5. Real time optimization
+# Real-time prompt optimization is an emerging technology that provides instant feedback on prompt effectiveness. This dynamic approach helps refine prompts for better outcomes.
+
+# 6. Active Prompting
+# Active prompting allows for dynamic adjustment of prompts based on user interaction and feedback. This adaptive approach enables AI models to refine their responses in real-time throughout an interaction.
+
+#7. Generated Knowledge
+# Before generating answer it generates some relevant knowledge before answering questions
